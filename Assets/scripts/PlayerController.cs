c5381527cb441a8e25a411166e703a362c446e3d
António José da Fonseca <antoniojosefonseca@gmail.com>
2020-01-27 23:09:18 +0000

    3Commit

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,38 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D selfRigidbody;
-    private bool MoveLeft;
-    private bool MoveRight;
-    private bool MoveDown;
-    private bool MoveUp;
+
+   
     public float speed;
+    public float JumpForce;
+    public Transform FeetRectangleTopLeft;
+    public Transform FeetRectangleBottomRight;
+    public LayerMask GroundLayer;
+    private bool isGrounded = false;
+    private bool canJump = false;
+    private Animator selfAnimator;
+    private float horizontalMovement;
     // Start is called before the first frame update
     void Start()
 
     {
         selfRigidbody = GetComponent<Rigidbody2D>();
+        selfAnimator = GetComponent<Animator>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-     MoveLeft=Input.GetKey (KeyCode.A);
-        MoveDown= Input.GetKey(KeyCode.S);
-        MoveRight= Input.GetKey(KeyCode.D);
-        MoveUp=Input.GetKey(KeyCode.W);
+        horizontalMovement = Input.GetAxis("Horizontal");
+        
+        
+        if (Input.GetAxis("Jump") > 0.1f && isGrounded)
+        {
+            canJump = true;
+        }
+        if (Mathf.Abs(horizontalMovement) > 0.1f)
+        {
+            selfAnimator.SetBool("Runs", true);
+
+        }
+        else
+        {
+            selfAnimator.SetBool("Runs", false);
+        }
+        if ((horizontalMovement > 0.1f && transform.localScale.x < 0) ||
+        (horizontalMovement < -0.1f && transform.localScale.x > 0))
+        {
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }
     }
+
     // Fixed update is called every x to x ms
       private void FixedUpdate()
     {
+        isGrounded = Physics2D.OverlapArea(FeetRectangleTopLeft.position, FeetRectangleBottomRight.position,
+            GroundLayer);
+
+        if (canJump)
+        {
+            canJump = false;
+            isGrounded = false;
+            selfRigidbody.AddForce(new Vector2(0, 1) * JumpForce, ForceMode2D.Impulse);
+        }
         
-        float currSpeed = 0;
-        if (MoveLeft) currSpeed -= speed;
-        if (MoveRight) currSpeed += speed;
-        selfRigidbody.velocity = new Vector2(currSpeed,selfRigidbody.velocity.y);
+        
+        selfRigidbody.velocity = new Vector2(speed * horizontalMovement, selfRigidbody.velocity.y);
+
     }    
 
     }
